556edd621dc57ffe0c5f7cc1c39236e7284180a6
eSKa-0 <166024351+eSKa-0@users.noreply.github.com>
2025-08-03 23:19:57 +0200

    Create main.c

---
+++ src/main.c
@@ -0,0 +1,23 @@
+#include <3ds.h>
+#include <stdio.h>
+
+int main() {
+    gfxInitDefault();
+    consoleInit(GFX_TOP, NULL);
+
+    printf("Hello, 3DS World!");
+
+    while (aptMainLoop()) {
+        hidScanInput();
+        u32 kDown = hidKeysDown();
+
+        if (kDown & KEY_START) break; // Exit with START button
+
+        gfxFlushBuffers();
+        gfxSwapBuffers();
+        gspWaitForVBlank();
+    }
+
+    gfxExit();
+    return 0;
+}
